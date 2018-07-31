1fc37a51df8b868c91580f537a42b792ba45bf9f
NightAxeblade <uddhav.navneeth2017@vitstudent.ac.in>
2018-07-31 22:24:49 +0530

    Data of user is being saved

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const hbs = require('hbs');
+const nodemailer = require('nodemailer');
 
 var {mongoose} = require('./server/db/mongoose');
-var {info} = require('./server/models/info');
+var {Info} = require('./server/models/info');
 
 var bodyParser = require('body-parser');
 
 var app = express();
 var port = 3000;
 
-app.use(bodyParser.json());
+app.set('view engine', hbs);
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get('/', (req, res) => {
     res.render('main.hbs');
 });
 
 app.post('/submit', (req, res) => {
-    var info = new info(req.body);
+    console.log(req.body);
+    //res.send(req.body);
+    var info = new Info(req.body);
     info.save().then((doc) => {
-        res.status(200).send(`saved succesfully 
-        ${doc}`);
+        res.status(200).render('success.hbs');
     }, (e) => {
-        res.status(400).send(`error occurred while saving
-        ${e}  `);
+        res.status(400).render('error.hbs');
     });
 });
+
+app.post('/send', (req, res) => {
+
+})
 
 
 app.listen(3000);
